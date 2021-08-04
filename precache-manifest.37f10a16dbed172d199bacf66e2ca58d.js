15cf3d45207cefa665d144b4ac07c42408675789
Jonathan Jesus <drigues90@gmai.com>
2021-08-04 16:46:48 -0300

    Updates

---
+++ precache-manifest.37f10a16dbed172d199bacf66e2ca58d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75896d31418210ff13b20204ac3287d1",
+    "url": "/drigues90.github.io/aluraflix-front/index.html"
+  },
+  {
+    "revision": "e500cc061a3553faf2f5",
+    "url": "/drigues90.github.io/aluraflix-front/static/css/main.43b442d7.chunk.css"
+  },
+  {
+    "revision": "da4fe0a5f432491509a9",
+    "url": "/drigues90.github.io/aluraflix-front/static/js/2.4b912ece.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/drigues90.github.io/aluraflix-front/static/js/2.4b912ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e500cc061a3553faf2f5",
+    "url": "/drigues90.github.io/aluraflix-front/static/js/main.2abbb770.chunk.js"
+  },
+  {
+    "revision": "fdbfc1a174811a735184",
+    "url": "/drigues90.github.io/aluraflix-front/static/js/runtime-main.38476d19.js"
+  },
+  {
+    "revision": "b1f5d75a009bbda15bd3ab76ed17cc50",
+    "url": "/drigues90.github.io/aluraflix-front/static/media/Logo.b1f5d75a.png"
+  }
+]);
